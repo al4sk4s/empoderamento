d0ac51b0eb64ee4b45d09f1c130e987f75a1bcae
gamer_0572 <gamerzueiro9000@gmail.com>
2022-08-21 19:52:31 -0300

    deletei algo

--- detect.js
+++
@@ -1,5 +0,0 @@
-const isMobile = function(){
-    const match = window.matchMedia('(pointer:coarse)');
-    return (match && match.matches);
-}
-console.log(`${isMobile() ? 'Mobile' : 'not mobile'}`);
